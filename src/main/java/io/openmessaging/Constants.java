1dc7f923de5b14e6ed2d5febb35b304abea3ccef
Yanbo Liang <git@liangyanbo.com>
2019-08-12 15:33:04 +0800

    commit

--- src/main/java/io/openmessaging/Constants.java
+++ src/main/java/io/openmessaging/Constants.java
@@ -1,14 +1,17 @@
 package io.openmessaging;
 
 public class Constants {
-//    static int Message_Size = 24;
+//        static int Message_Size = 24;
     static int Message_Size = 50;
 
     static int Message_Batch_Size = 5000000;
     static int Index_Skip_Size = Message_Size * 1000;
 
-//    static String Path = "/Users/yanbo.liang/test/hive";
-        static String Path = "/alidata1/race2019/data/hive";
+//    static String Messages = "/Users/yanbo.liang/test/messages";
+//    static String Messages_Without_Data = "/Users/yanbo.liang/test/messagesWithoutData";
+
+    static String Messages = "/alidata1/race2019/data/messages";
+    static String Messages_Without_Data = "/alidata1/race2019/data/messagesWithoutData";
 
     static long Direct_Memory_Size = 2L * 1024 * 1024 * 1024;
     static long Direct_Write_Buffer_Size = 256 * 1024 * 1024;
