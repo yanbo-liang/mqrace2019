003122be2e18d7191ff7a6387a64769aefb38412
Yanbo Liang <git@liangyanbo.com>
2019-08-21 01:30:27 +0800

    Revert: commit

--- src/main/java/io/openmessaging/Constants.java
+++ src/main/java/io/openmessaging/Constants.java
@@ -2,20 +2,18 @@ package io.openmessaging;
 
 public class Constants {
 
-//    static int Message_Size = 24;
-//    static String Path = "/Users/yanbo.liang/test/message";
-//    static String Header_Path = "/Users/yanbo.liang/test/message_header";
+    static int Message_Size = 24;
+    static String Path = "/Users/yanbo.liang/test/message";
+    static String Header_Path = "/Users/yanbo.liang/test/message_header";
 
-    static int Message_Size = 50;
-    static String Path = "/alidata1/race2019/data/message";
-    static String Header_Path = "/alidata1/race2019/data/message_header";
+//    static int Message_Size = 50;
+//    static String Path = "/alidata1/race2019/data/message";
+//    static String Header_Path = "/alidata1/race2019/data/message_header";
 
     static int Message_Batch_Size = 5000000;
-    static int Index_Skip_Size = Message_Size * 1000;
 
     static long Direct_Memory_Size = 2L * 1024 * 1024 * 1024;
     static long Direct_Write_Buffer_Size = 256 * 1024 * 1024;
-    static long Direct_Write_Small_Buffer_Size = 128 * 1024 * 1024;
 
     static long Direct_Read_Buffer_Size = 40L * 1024 * 1024;
 
