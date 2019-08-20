5adc632c55a7d47a0d576d2b4ca664d3245c6ef1
Yanbo Liang <git@liangyanbo.com>
2019-08-20 20:29:04 +0800

    Revert: commit

--- src/main/java/io/openmessaging/CompressUtil.java
+++ src/main/java/io/openmessaging/CompressUtil.java
@@ -1,25 +1,5 @@
 package io.openmessaging;
 
-import java.nio.ByteBuffer;
-import java.util.zip.Deflater;
-import java.util.zip.Inflater;
 
 public class CompressUtil {
-    private static Deflater compresser = new Deflater(1);
-    private static Inflater uncompresser = new Inflater();
-
-//    public static int compress(byte[] uncompressed, int uStart, int uLength, byte[] compressed, int cStart, int cLength) {
-//        compresser.reset();
-//        compresser.setInput(uncompressed, uStart, uLength);
-//        compresser.finish();
-//        return compresser.deflate(compressed, cStart, cLength);
-//    }
-    public static int compress(ByteBuffer uncompressed, ByteBuffer compressed) {
-        compresser.reset();
-        compresser.setStrategy(0);
-        uncompressed.flip();
-        compresser.setInput(uncompressed.array(), 0, uncompressed.limit());
-        compresser.finish();
-        return compresser.deflate(compressed.array(), compressed.position(), compressed.capacity()-compressed.position());
-    }
 }
