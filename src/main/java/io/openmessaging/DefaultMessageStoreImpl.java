3a36d170cd320c2956a4e1b14d75f897bd3c233b
Yanbo Liang <git@liangyanbo.com>
2019-08-24 03:09:06 +0800

    Revert: commit

--- src/main/java/io/openmessaging/DefaultMessageStoreImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageStoreImpl.java
@@ -61,58 +61,58 @@ public class DefaultMessageStoreImpl extends MessageStore {
 
     ConcurrentHashMap<Long,LocalInfo> map = new ConcurrentHashMap<>();
 int a=0;
-    @Override
-    void put(Message message) {
-        try {
-            LocalInfo localInfo = local.get();
-            if (localInfo == null) {
-                localInfo = new LocalInfo();
-                local.set(localInfo);
-                map.put(Thread.currentThread().getId(),localInfo);
-            }
-toLong(localInfo.i,message,localInfo.data);
-            int i = ++localInfo.i;
-            if (i == 500000) {
-                if (!threadCountInit.get()) {
-                    if (threadCountInit.compareAndSet(false, true)) {
-                        while (waitThread.get() == (map.size() - 1)) {
-
-                        }
-                        barrier = new CyclicBarrier(map.size(), new Runnable() {
-                            @Override
-                            public void run() {
-                                map.forEach((x,y)->{
-                                    y.i=0;
-                                });
-                                a++;
-                                if (a>100){
-                                    System.exit(-1);
-                                }
-                                System.out.println(System.currentTimeMillis()-s);
-                                s=System.currentTimeMillis();
-                            }
-                        });
-                        synchronized (this) {
-                            this.notifyAll();
-                        }
-                    }
-                }
-                if (barrier == null) {
-                    synchronized (this) {
-                        waitThread.incrementAndGet();
-                        this.wait();
-                    }
-                }
-                barrier.await();
-            }
-
-
-        } catch (
-                Exception e) {
-            e.printStackTrace();
-        }
-
-    }
+//    @Override
+//    void put(Message message) {
+//        try {
+//            LocalInfo localInfo = local.get();
+//            if (localInfo == null) {
+//                localInfo = new LocalInfo();
+//                local.set(localInfo);
+//                map.put(Thread.currentThread().getId(),localInfo);
+//            }
+//toLong(localInfo.i,message,localInfo.data);
+//            int i = ++localInfo.i;
+//            if (i == 500000) {
+//                if (!threadCountInit.get()) {
+//                    if (threadCountInit.compareAndSet(false, true)) {
+//                        while (waitThread.get() == (map.size() - 1)) {
+//
+//                        }
+//                        barrier = new CyclicBarrier(map.size(), new Runnable() {
+//                            @Override
+//                            public void run() {
+//                                map.forEach((x,y)->{
+//                                    y.i=0;
+//                                });
+//                                a++;
+//                                if (a>100){
+//                                    System.exit(-1);
+//                                }
+//                                System.out.println(System.currentTimeMillis()-s);
+//                                s=System.currentTimeMillis();
+//                            }
+//                        });
+//                        synchronized (this) {
+//                            this.notifyAll();
+//                        }
+//                    }
+//                }
+//                if (barrier == null) {
+//                    synchronized (this) {
+//                        waitThread.incrementAndGet();
+//                        this.wait();
+//                    }
+//                }
+//                barrier.await();
+//            }
+//
+//
+//        } catch (
+//                Exception e) {
+//            e.printStackTrace();
+//        }
+//
+//    }
 
     @Override
     List<Message> getMessage(long aMin, long aMax, long tMin, long tMax) {
@@ -193,53 +193,53 @@ toLong(localInfo.i,message,localInfo.data);
     }
 
 
-//    @Override
-//    void put(Message message) {
-//        try {
-//            concurrentPut.incrementAndGet();
-//            int count = messageCount.getAndIncrement();
-//            if (count < batchSize - 1) {
-//                messageToBuffer(count, message);
-//                concurrentPut.decrementAndGet();
-//
-//            } else if (count == batchSize - 1) {
-//                messageToBuffer(count, message);
-//                concurrentPut.decrementAndGet();
-//
-//                while (concurrentPut.get() != 0) {
-//                }
-//
-////                MessageWriter.write(new MessageWriterTask(messageBuffer,Constants.Message_Batch_Size));
-////                messageBuffer=new long[Constants.Message_Buffer_Size];
-//
-//                System.out.println(System.currentTimeMillis()-s);
-//                s=System.currentTimeMillis();
-//                messageCount.getAndUpdate(x -> 0);
-//                synchronized (this) {
-//                    this.notifyAll();
-//                }
-//            } else if (count > batchSize - 1) {
-//                synchronized (this) {
-//                    concurrentPut.decrementAndGet();
-//                    this.wait();
-//                }
-//
-//                concurrentPut.incrementAndGet();
-//                count = messageCount.getAndIncrement();
-//                messageToBuffer(count, message);
-//                concurrentPut.decrementAndGet();
-//            }
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
+    @Override
+    void put(Message message) {
+        try {
+            concurrentPut.incrementAndGet();
+            int count = messageCount.getAndIncrement();
+            if (count < batchSize - 1) {
+                messageToBuffer(count, message);
+                concurrentPut.decrementAndGet();
+
+            } else if (count == batchSize - 1) {
+                messageToBuffer(count, message);
+                concurrentPut.decrementAndGet();
+
+                while (concurrentPut.get() != 0) {
+                }
+
+//                MessageWriter.write(new MessageWriterTask(messageBuffer,Constants.Message_Batch_Size));
+//                messageBuffer=new long[Constants.Message_Buffer_Size];
+
+                System.out.println(System.currentTimeMillis()-s);
+                s=System.currentTimeMillis();
+                messageCount.getAndUpdate(x -> 0);
+                synchronized (this) {
+                    this.notifyAll();
+                }
+            } else if (count > batchSize - 1) {
+                synchronized (this) {
+                    concurrentPut.decrementAndGet();
+                    this.wait();
+                }
+
+                concurrentPut.incrementAndGet();
+                count = messageCount.getAndIncrement();
+                messageToBuffer(count, message);
+                concurrentPut.decrementAndGet();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     private void messageToBuffer(int count, Message message) {
         int startIndex = messageBufferStart + count * Constants.Message_Long_size;
         messageBuffer[startIndex] = message.getT();
         messageBuffer[startIndex + 1] = message.getA();
 //        messageBuffer[startIndex+2]=ByteBuffer.wrap(message.getBody()).getLong();
-        LongArrayUtils.byteArrayToLongArray(messageBuffer, startIndex + 2, message.getBody());
+//        LongArrayUtils.byteArrayToLongArray(messageBuffer, startIndex + 2, message.getBody());
 //
 //        ByteBuffer byteBuffer = ByteBuffer.allocate(8);
 //        LongArrayUtils.longArraytoByteBuffer(messageBuffer, startIndex + 2, byteBuffer);
