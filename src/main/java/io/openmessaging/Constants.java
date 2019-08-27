5d60a214ea021f32ece38f33518d57d64bb4d0f4
Yanbo Liang <git@liangyanbo.com>
2019-08-28 02:25:56 +0800

    Revert: commit

--- src/main/java/io/openmessaging/Constants.java
+++ src/main/java/io/openmessaging/Constants.java
@@ -2,15 +2,15 @@ package io.openmessaging;
 
 public class Constants {
 
-    public static int Message_Size = 24;
-    public static int Thread_Count = 12;
-    public static String Message_Path = "/Users/yanbo.liang/test/message";
-    public static String A_Path = "/Users/yanbo.liang/test/header";
-
-//    public static int Message_Size = 50;
+//    public static int Message_Size = 24;
 //    public static int Thread_Count = 12;
-//    public static String Message_Path = "/alidata1/race2019/data/message";
-//    public static String A_Path = "/alidata1/race2019/data/header";
+//    public static String Message_Path = "/Users/yanbo.liang/test/message";
+//    public static String A_Path = "/Users/yanbo.liang/test/header";
+
+    public static int Message_Size = 50;
+    public static int Thread_Count = 12;
+    public static String Message_Path = "/alidata1/race2019/data/message";
+    public static String A_Path = "/alidata1/race2019/data/header";
 
     public static int Message_Batch_Size = 5000000;
     public static int Message_Buffer_Size = Message_Batch_Size * Message_Size;
