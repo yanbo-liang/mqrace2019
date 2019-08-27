8eedd186e690674f01fd2a6c9c0b2b59c3e50cca
Yanbo Liang <git@liangyanbo.com>
2019-08-28 00:19:14 +0800

    Revert: commit

--- src/main/java/io/openmessaging/unsafe/UnsafePut.java
+++ src/main/java/io/openmessaging/unsafe/UnsafePut.java
@@ -24,9 +24,9 @@ public class UnsafePut {
             } else if (count == Constants.Message_Batch_Size - 1) {
                 putMessage(count, message);
 
-                latch.await(1, TimeUnit.SECONDS);
+                System.out.println(latch.await(1, TimeUnit.SECONDS));
                 System.out.println(++batchCount);
-                unsafeBuffer.setLimit(count * Constants.Message_Size);
+                unsafeBuffer.setLimit((count+1) * Constants.Message_Size);
                 UnsafeWriter.write(unsafeBuffer);
                 if (batchCount % 2 == 1) {
                     unsafeBuffer = unsafeBuffer2;
