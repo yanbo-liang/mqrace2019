6346f281a105542c701cf8c9bf1fd6496f2f2686
Yanbo Liang <git@liangyanbo.com>
2019-08-28 19:50:22 +0800

    Revert: commit

--- src/main/java/io/openmessaging/MessageReader.java
+++ src/main/java/io/openmessaging/MessageReader.java
@@ -9,35 +9,45 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 
 public class MessageReader {
-    private static FileChannel messageChannel, aChannel;
+    private static ThreadLocal<FileChannel> messageChannel = new ThreadLocal<>();
+    private static ThreadLocal<FileChannel> aChannel = new ThreadLocal<>();
 
     static {
-        try {
-            messageChannel = FileChannel.open(Paths.get(Constants.Message_Path), StandardOpenOption.CREATE, StandardOpenOption.READ);
-            aChannel = FileChannel.open(Paths.get(Constants.A_Path), StandardOpenOption.CREATE, StandardOpenOption.READ);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+//        try {
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
     }
 
     public static void read(ByteBuffer buffer, long tMin, long tMax) throws Exception {
-        long messageStart = PartitionIndex.getMessageStart(tMin) / Constants.Message_Size*42;
-        long messageEnd = PartitionIndex.getMessageEnd(tMax)/ Constants.Message_Size*42;
-        asyncRead(buffer, messageChannel, messageStart, messageEnd - messageStart);
+        FileChannel fileChannel = messageChannel.get();
+        if (fileChannel == null) {
+            fileChannel = FileChannel.open(Paths.get(Constants.Message_Path), StandardOpenOption.READ);
+            messageChannel.set(fileChannel);
+        }
+        long messageStart = PartitionIndex.getMessageStart(tMin) / Constants.Message_Size * 42;
+        long messageEnd = PartitionIndex.getMessageEnd(tMax) / Constants.Message_Size * 42;
+        asyncRead(buffer, fileChannel, messageStart, messageEnd - messageStart);
     }
 
     public static void fastRead(ByteBuffer buffer, long tMin, long tMax) throws Exception {
+        FileChannel fileChannel = aChannel.get();
+        if (fileChannel == null) {
+            fileChannel = FileChannel.open(Paths.get(Constants.A_Path), StandardOpenOption.CREATE, StandardOpenOption.READ);
+            aChannel.set(fileChannel);
+        }
         long aStart = PartitionIndex.getAStart(tMin);
         long aEnd = PartitionIndex.getAEnd(tMax);
-        asyncRead(buffer, aChannel, aStart, aEnd - aStart);
+        asyncRead(buffer, fileChannel, aStart, aEnd - aStart);
     }
 
     private static void asyncRead(ByteBuffer buffer, FileChannel channel, long start, long length) throws Exception {
         long readStart = System.currentTimeMillis();
 //        synchronized (buffer) {
-            buffer.limit((int) length);
+        buffer.limit((int) length);
 //            channel.read(buffer, start, buffer, new WriteCompletionHandler());
-                    channel.read(buffer, start);
+        channel.read(buffer, start);
 
 //            buffer.wait();
 //        }
