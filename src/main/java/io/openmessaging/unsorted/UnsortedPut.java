12e3ab3df981a6d13a9dc8a1ee04db8ccf1f5a36
Yanbo Liang <git@liangyanbo.com>
2019-08-30 13:23:20 +0800

    commit

--- src/main/java/io/openmessaging/unsorted/UnsortedPut.java
+++ src/main/java/io/openmessaging/unsorted/UnsortedPut.java
@@ -6,13 +6,20 @@ import io.openmessaging.unsafe.UnsafeBuffer;
 import io.openmessaging.unsafe.UnsafeWriter;
 import org.omg.CORBA.TCKind;
 
+import java.nio.MappedByteBuffer;
+import java.nio.channels.AsynchronousFileChannel;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.concurrent.CyclicBarrier;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class UnsortedPut {
-    private static UnsafeBuffer unsafeBuffer = UnsortedBufferManager.borrowBuffer();
+    private static int totalByteWritten = 0;
+    private static FileChannel fileChannel;
+    private static MappedByteBuffer unsafeBuffer;
     private static AtomicInteger count = new AtomicInteger(0);
     private static long min = 0;
     private static long max = 0;
@@ -21,17 +28,35 @@ public class UnsortedPut {
         if (count.get() * Constants.Message_Size > UnsortedConstants.Buffer_Size) {
             System.out.println("fucker");
             System.exit(1);
-        }else{
+        } else {
             System.out.println(count.get());
         }
         min += UnsortedConstants.Partition_Size;
         max += UnsortedConstants.Partition_Size;
-        unsafeBuffer.position(count.get() * (Constants.Message_Size-8));
+        unsafeBuffer.force();
         unsafeBuffer.flip();
-        UnsortedWriter.write(unsafeBuffer);
-        unsafeBuffer = UnsortedBufferManager.borrowBuffer();
+        totalByteWritten += count.get() * (Constants.Message_Size - 8);
+        try {
+            unsafeBuffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, totalByteWritten, UnsortedConstants.Buffer_Size);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//        unsafeBuffer.position(count.get() * (Constants.Message_Size - 8));
+//        unsafeBuffer.flip();
+//        UnsortedWriter.write(unsafeBuffer);
+//        unsafeBuffer = UnsortedBufferManager.borrowBuffer();
         count.set(0);
     });
+
+    static {
+        try {
+            fileChannel = FileChannel.open(Paths.get(Constants.Message_Path), StandardOpenOption.CREATE, StandardOpenOption.READ, StandardOpenOption.WRITE);
+            unsafeBuffer = fileChannel.map(FileChannel.MapMode.READ_WRITE, totalByteWritten, UnsortedConstants.Buffer_Size);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void put(Message message) {
         if (!init.get()) {
@@ -51,12 +76,15 @@ public class UnsortedPut {
                 break;
             }
         }
-        int index = count.getAndIncrement() * (Constants.Message_Size-8);
+        int index = count.getAndIncrement() * (Constants.Message_Size - 8);
 //        unsafeBuffer.putLong(index, message.getT());
 //        unsafeBuffer.putLong(index + 8, message.getA());
 //        unsafeBuffer.put(index + 16, message.getBody());
-        unsafeBuffer.putLong(index , message.getA());
-        unsafeBuffer.put(index + 8, message.getBody());
+        unsafeBuffer.putLong(index, message.getA());
+        for (int i = 0; i < Constants.Message_Size - 16; i++) {
+            unsafeBuffer.put(index + 8 + i, message.getBody()[i]);
+
+        }
     }
 
 
