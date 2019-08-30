de297e470a8dab43666d1682c077444afe522cb9
Yanbo Liang <git@liangyanbo.com>
2019-08-30 13:10:16 +0800

    commit

--- src/main/java/io/openmessaging/unsorted/UnsortedConstants.java
+++ src/main/java/io/openmessaging/unsorted/UnsortedConstants.java
@@ -3,7 +3,7 @@ package io.openmessaging.unsorted;
 import io.openmessaging.Constants;
 
 class UnsortedConstants {
-    static int Partition_Size = 5000000;
-    static int Buffer_Size = (int)(2.5 * Partition_Size * Constants.Message_Size);
+    static int Partition_Size = 4000000;
+    static int Buffer_Size = (int)(2.5 * Partition_Size * (Constants.Message_Size-8));
     static int Buffer_Queue_Size = (int)(Constants.Direct_Memory_Size / Buffer_Size);
 }
