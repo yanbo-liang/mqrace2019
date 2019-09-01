e1d64289ec63d01d13980a98e4dad65793f51b34
Yanbo Liang <git@liangyanbo.com>
2019-09-01 21:48:34 +0800

    final

--- src/main/java/io/openmessaging/Constants.java
+++ src/main/java/io/openmessaging/Constants.java
@@ -2,13 +2,13 @@ package io.openmessaging;
 
 public class Constants {
 
-    public static int Message_Size = 24;
-    public static String Body_Path = "/Users/yanbo.liang/test/message";
-    public static String A_Path = "/Users/yanbo.liang/test/header";
+//    public static int Message_Size = 24;
+//    public static String Body_Path = "/Users/yanbo.liang/test/message";
+//    public static String A_Path = "/Users/yanbo.liang/test/header";
 
-//    public static int Message_Size = 50;
-//    public static String Body_Path = "/alidata1/race2019/data/message";
-//    public static String A_Path = "/alidata1/race2019/data/header";
+    public static int Message_Size = 50;
+    public static String Body_Path = "/alidata1/race2019/data/message";
+    public static String A_Path = "/alidata1/race2019/data/header";
 
     public static int Thread_Count = 12;
     public static int Body_Size = Message_Size - 16;
