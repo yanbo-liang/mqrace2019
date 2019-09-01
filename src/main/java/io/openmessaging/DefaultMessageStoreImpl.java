d48847c8bda97ac9e17adff218815d742f5521ee
Yanbo Liang <git@liangyanbo.com>
2019-09-01 23:10:00 +0800

    final

--- src/main/java/io/openmessaging/DefaultMessageStoreImpl.java
+++ src/main/java/io/openmessaging/DefaultMessageStoreImpl.java
@@ -25,21 +25,21 @@ public class DefaultMessageStoreImpl extends MessageStore {
 
     @Override
     void put(Message message) {
-        try {
-//            Class<?> c = Class.forName("java.nio.Bits");
-//            Field maxMemory = c.getDeclaredField("maxMemory");
-//            maxMemory.setAccessible(true);
-//            Field reservedMemory = c.getDeclaredField("reservedMemory");
-//            reservedMemory.setAccessible(true);
-//            Long maxMemoryValue = (Long)maxMemory.get(null);
-//            Long reservedMemoryValue = (Long)reservedMemory.get(null);
-
-            System.out.println(VM.maxDirectMemory());
-System.exit(1);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+//        try {
+////            Class<?> c = Class.forName("java.nio.Bits");
+////            Field maxMemory = c.getDeclaredField("maxMemory");
+////            maxMemory.setAccessible(true);
+////            Field reservedMemory = c.getDeclaredField("reservedMemory");
+////            reservedMemory.setAccessible(true);
+////            Long maxMemoryValue = (Long)maxMemory.get(null);
+////            Long reservedMemoryValue = (Long)reservedMemory.get(null);
+//
+//            System.out.println(VM.maxDirectMemory());
+//System.exit(1);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
 
         try {
             MessagePut.put(message);
